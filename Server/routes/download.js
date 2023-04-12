b02ecc58d6d73a381a65b24feb53affd6a59115f
Cloudocity <valentinkoenig249@gmail.com>
2023-04-12 08:37:45 +0200

    Fix a filename bug
    If a video had any kind of special character in its name, the server
    would crash while naming the associated file.

--- Server/routes/download.js
+++ Server/routes/download.js
@@ -1,12 +1,13 @@
 var express = require('express');
 var ytdl = require('ytdl-core');
+var ytpl = require('ytpl');
 var router = express.Router();
 
 
 /* GET download. */
 router.get('/', async (req, res, next) => {
   ytdl.getInfo(req.query.url).then(info => {
-    res.header('Content-Disposition', `attachment; filename=${info.videoDetails.title}.webm`);
+    res.header('Content-Disposition', `attachment; filename=${(info.videoDetails.title).replace(/[^\x00-\x7F]/g, "")}.webm`);
     ytdl(req.query.url, {filter: 'audioonly', format: 'webm'}).pipe(res);
   })
 });
