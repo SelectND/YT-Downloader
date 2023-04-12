9c2e99b7b0e406221c759c0dcef983fadab7eedf
Cloudocity <100362519+SelectND@users.noreply.github.com>
2023-04-12 13:00:29 +0200

    Remove router bug
    The app.js included a router that is for a future feature
    an not yet present.

--- Server/app.js
+++ Server/app.js
@@ -6,7 +6,6 @@ var cors = require('cors');
 
 var indexRouter = require('./routes/index');
 var downloadRouter = require('./routes/download');
-var playlistRouter = require('./routes/playlist');
 
 var app = express();
 
@@ -19,6 +18,5 @@ app.use(cors());
 
 app.use('/', indexRouter);
 app.use('/download', downloadRouter);
-app.use('/playlist', playlistRouter);
 
 module.exports = app;
