c8d51fe8b2c74b95585c332b6f27d6b2a5f81459
Cloudocity <valentinkoenig249@gmail.com>
2023-04-12 12:27:00 +0200

    Download all files in mp3 format
    For easier playback of the files, all files will
    get downloaded in the .mp3 format. Also some audio
    players don't support the .webm format.

--- Server/app.js
+++ Server/app.js
@@ -6,17 +6,19 @@ var cors = require('cors');
 
 var indexRouter = require('./routes/index');
 var downloadRouter = require('./routes/download');
+var playlistRouter = require('./routes/playlist');
 
 var app = express();
 
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(cors());
 
 app.use('/', indexRouter);
 app.use('/download', downloadRouter);
+app.use('/playlist', playlistRouter);
 
 module.exports = app;
